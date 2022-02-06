523dc6b80e443163cb9a7da7ebb561fac771a441
RainyBoots0 <99148255+RainyBoots0@users.noreply.github.com>
2022-02-07 00:11:29 +0100

    Create Importer.lua

---
+++ Importer.lua
@@ -0,0 +1,7 @@
+local MODULE_PARENT_URL = "https://raw.githubusercontent.com/RainyBoots0/Modules/main/List"
+local function Import(ModuleName)
+	return loadstring((game.HttpGet(game, MODULE_PARENT_URL .. ("/" .. (ModuleName .. ".lua")))))().main
+end
+return {
+	Import = Import,
+}

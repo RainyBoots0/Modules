969cd4b0a6fc4471e00aeeeefb25e60eb30fdb13
RainyBoots0 <99148255+RainyBoots0@users.noreply.github.com>
2022-02-07 00:34:16 +0100

    Add to global

--- Importer.lua
+++ Importer.lua
@@ -2,6 +2,7 @@ local MODULE_PARENT_URL = "https://raw.githubusercontent.com/RainyBoots0/Modules
 local function Import(ModuleName)
 	return loadstring((game.HttpGet(game, MODULE_PARENT_URL .. ("/" .. (ModuleName .. ".lua")))))().main
 end
+getgenv().Import = Import
 return {
 	Import = Import,
 }
